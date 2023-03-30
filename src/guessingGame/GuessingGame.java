1dc791b08f5718ae5ddc3bb7120bec9f6325cffa
lawtonhself <lawtonhself@hotmail.com>
2023-03-30 13:20:24 -0400

    Started work with GUI
    Created a GUI with textField, button, and 2 labels. Working on adding the code behind the guessing game part

--- src/guessingGame/GuessingGame.java
+++ src/guessingGame/GuessingGame.java
@@ -1,12 +1,64 @@
 package guessingGame;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class GuessingGame extends JFrame {
+	private JTextField txtGuess;
+	private JLabel lblOutput;
+	private int theNumber;
+	
+	public void checkGuess() {
+		String guessText = txtGuess.getText();
+		String message = "";
+		int guess = Integer.parseInt(guessText);
+		if(guess < theNumber) {
+			message = guess + " is too low. Try again.";
+		} 
+		else if(guess > theNumber) {
+			message = guess + " is too high. Try again.";
+		}
+		else
+			message = guess + " is correct. You win!";
+	}
+	
+	public GuessingGame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Lawton's Hi-Lo Guessing Game");
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Lawton's Hi-Lo Guessing Game");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(0, 36, 434, 27);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Guess a number between 1 and 100:");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_1.setBounds(90, 98, 185, 14);
+		getContentPane().add(lblNewLabel_1);
+		
+		txtGuess = new JTextField();
+		txtGuess.setBounds(285, 95, 60, 20);
+		getContentPane().add(txtGuess);
+		txtGuess.setColumns(10);
+		
+		JButton btnGuess = new JButton("Guess!");
+		btnGuess.setBounds(172, 149, 89, 23);
+		getContentPane().add(btnGuess);
+		
+		lblOutput= new JLabel("Enter a number above and click Guess!");
+		lblOutput.setHorizontalAlignment(SwingConstants.CENTER);
+		lblOutput.setBounds(116, 208, 203, 14);
+		getContentPane().add(lblOutput);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//hi
+		
 	}
-
 }
