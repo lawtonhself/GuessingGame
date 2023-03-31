f701fadf6b0056d123ee820da0de750a2e8a0db5
lawtonhself <lawtonhself@hotmail.com>
2023-03-31 08:21:56 -0400

    Exception handling
    -added try catch block when user guesses to make sure they but in only integers

--- src/guessingGame/GuessingGame.java
+++ src/guessingGame/GuessingGame.java
@@ -18,59 +18,68 @@ public class GuessingGame extends JFrame {
 	private JLabel lblOutput;
 	//declares a new Int that will be used for the number that the user is trying to guess
 	private int theNumber;
-	
+
 	//method to check the guess of the user
 	public void checkGuess() {
 		//declares a new string that is assigned to the text that the user inputs into the text field "txtGuess"
 		String guessText = txtGuess.getText();
 		//declares a string that will be used to give a message to the player on if they guessed the number or not and what to do
 		String message = "";
-		//declares an Int that will take the string text from the user and parse it to Int
-		int guess = Integer.parseInt(guessText);
-		//if statement for if user's guess is too low, too high, or right
-		if(guess < theNumber) {
-			message = guess + " is too low. Try again.";
-		} 
-		else if(guess > theNumber) {
-			message = guess + " is too high. Try again.";
+
+		//try block for exception handling
+		try {
+			//declares an Int that will take the string text from the user and parse it to Int
+			int guess = Integer.parseInt(guessText);
+			//if statement for if user's guess is too low, too high, or right
+			if(guess < theNumber) {
+				message = guess + " is too low. Try again.";
+			} 
+			else if(guess > theNumber) {
+				message = guess + " is too high. Try again.";
+			}
+			else {
+				message = guess + " is correct. You win! Let's play again!";
+				newGame();
+			}	
+			//catch block for exception handling
+		} catch (Exception e) {
+			message = "Enter a whole number between 1 and 100.";
+			//finally block for exception handling
+		} finally {
+			//will display the message that comes from the above if statement depending on the user's guess into the label "lblOutput"
+			lblOutput.setText(message);
+			//The request focus will bring the mouse back into the text field so that we can select all. Now the user doesnt have to select the text or delete it before typing in another answer.
+			//It will be selected automatically so once user starts typing, the text will be replaced with the new text 
+			txtGuess.requestFocus();
+			txtGuess.selectAll();
 		}
-		else {
-			message = guess + " is correct. You win! Let's play again!";
-			newGame();
-		}
-		//will display the message that comes from the above if statement depending on the user's guess into the label "lblOutput"
-		lblOutput.setText(message);
-		//The request focus will bring the mouse back into the text field so that we can select all. Now the user doesnt have to select the text or delete it before typing in another answer.
-		//It will be selected automatically so once user starts typing, the text will be replaced with the new text 
-		txtGuess.requestFocus();
-		txtGuess.selectAll();
 	}
-	
+
 	//method used for the game so that we can replay it
 	public void newGame() {
 		//assigns a random number from 1 to 100 that the user will have to guess
 		theNumber = (int)(Math.random() * 100 + 1);
 	}
-	
+
 	//method for displaying GUI and everything in it
 	public GuessingGame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Lawton's Hi-Lo Guessing Game");
 		getContentPane().setLayout(null);
-		
+
 		//label for title
 		JLabel lblNewLabel = new JLabel("Lawton's Hi-Lo Guessing Game");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBounds(0, 36, 434, 27);
 		getContentPane().add(lblNewLabel);
-		
+
 		//label to prompt user to guess a number between 1 and 100
 		JLabel lblNewLabel_1 = new JLabel("Guess a number between 1 and 100:");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblNewLabel_1.setBounds(46, 98, 229, 14);
 		getContentPane().add(lblNewLabel_1);
-		
+
 		//text field that user will input their guess
 		txtGuess = new JTextField();
 		txtGuess.addActionListener(new ActionListener() {
@@ -82,7 +91,7 @@ public class GuessingGame extends JFrame {
 		txtGuess.setBounds(285, 95, 60, 20);
 		getContentPane().add(txtGuess);
 		txtGuess.setColumns(10);
-		
+
 		//button for user to hit for their guess
 		JButton btnGuess = new JButton("Guess!");
 		btnGuess.addActionListener(new ActionListener() {
@@ -93,7 +102,7 @@ public class GuessingGame extends JFrame {
 		});
 		btnGuess.setBounds(172, 149, 89, 23);
 		getContentPane().add(btnGuess);
-		
+
 		//label to prompt user on what to do and how to play the game
 		lblOutput= new JLabel("Enter a number above and click Guess!");
 		lblOutput.setHorizontalAlignment(SwingConstants.CENTER);
